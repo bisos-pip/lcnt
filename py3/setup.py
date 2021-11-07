3a3a4f75244adb8fb66773b6833f98954b2eca79
Mohsen Banan <form@mohsen.banan.1.byname.net>
2021-11-06 23:05:08 -0700

    setup.py 0.9 -- cleanups + requirements.

--- py3/setup.py
+++ py3/setup.py
@@ -17,11 +17,14 @@ def longDescription():
 
 
 #__version__ = get_version('unisos/icm/__init__.py')
-__version__ = '0.8'
+__version__ = '0.9'
 
 
 requires = [
     'unisos.icm',
+    'pexpect',
+    'PyPDF4',
+    'requests',
 ]
 
 
